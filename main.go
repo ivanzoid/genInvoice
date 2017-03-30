f4cde29e25dcec88434ef93e29165227e9800297
Ivan Zezyulya <ivan@zoid.cc>
2017-03-30 15:59:02 +0600

    Mostly implemented generator

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
+	"text/template"
 
 	"gopkg.in/yaml.v2"
 )
@@ -16,6 +18,14 @@ var (
 	flagTemplateFilePath string
 	flagConfigFilePath   string
 	flagMode             string
+)
+
+const (
+	kInvoiceKey        = "invoice"
+	kDateKey           = "date"
+	kGenInvoiceKey     = "gen_invoice"
+	kGenDateCreatedKey = "gen_date_created"
+	kGenDateDueKey     = "gen_date_due"
 )
 
 func usage() {
@@ -53,7 +63,7 @@ func init() {
 // 	return
 // }
 
-func readInvoice(filePath string) (invoiceData map[interface{}]interface{}, err error) {
+func readInvoice(filePath string) (invoiceData map[string]interface{}, err error) {
 	bytes, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return
@@ -65,25 +75,25 @@ func readInvoice(filePath string) (invoiceData map[interface{}]interface{}, err 
 		return
 	}
 
-	invoiceData, ok := data.(map[interface{}]interface{})
+	invoiceUntilteredData, ok := data.(map[interface{}]interface{})
 	if !ok {
 		return nil, errors.New("Yaml root object is not dictionary")
+	}
+
+	invoiceData = make(map[string]interface{})
+
+	for key, value := range invoiceUntilteredData {
+		if keyAsString, ok := key.(string); ok {
+			invoiceData[keyAsString] = value
+		} else {
+			fmt.Fprintf(os.Stderr, "Key '%v' is not string - ignoring.\n", key)
+		}
 	}
 
 	return invoiceData, nil
 }
 
-func readTemplate(filePath string) (template string, err error) {
-	buffer, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return
-	}
-
-	template = string(buffer)
-	return
-}
-
-func produceInvoiceTable(invoiceValues [][]string) string {
+func produceInvoiceTable(invoiceValues [][]interface{}) string {
 	var invoiceTableBuffer bytes.Buffer
 
 	for i, lineValues := range invoiceValues {
@@ -106,6 +116,42 @@ func produceInvoiceTable(invoiceValues [][]string) string {
 	return invoiceTableBuffer.String()
 }
 
+func convertInvoiceTable(invoiceTable interface{}) (invoiceValues [][]interface{}, err error) {
+	if invoiceTable == nil {
+		return nil, errors.New("missing table")
+	}
+
+	invoiceTableAsArray, ok := invoiceTable.([]interface{})
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("type %T is not array", invoiceTable))
+	}
+
+	for i, value := range invoiceTableAsArray {
+		valueAsArray, ok := value.([]interface{})
+		if !ok {
+			return nil, errors.New(fmt.Sprintf("object at index %v with type %T is not array", i, invoiceTable))
+		}
+		invoiceValues = append(invoiceValues, valueAsArray)
+	}
+
+	return
+}
+
+func appendBrsToMultilineStringsInInvoiceData(invoiceData map[string]interface{}) {
+	for key, value := range invoiceData {
+		if valueAsString, ok := value.(string); ok {
+			lines := strings.Split(valueAsString, "\n")
+			if len(lines) >= 2 {
+				for i, line := range lines {
+					lines[i] = fmt.Sprintf("%v<br>", line)
+				}
+				linesAsString := strings.Join(lines, "\n")
+				invoiceData[key] = linesAsString
+			}
+		}
+	}
+}
+
 func main() {
 	flag.Parse()
 	if flag.NArg() < 1 {
@@ -117,21 +163,31 @@ func main() {
 	invoiceData, err := readInvoice(invoiceFilePath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Can't read invoice: %v\n", err)
+		return
 	}
 
-	fmt.Println(invoiceData)
+	appendBrsToMultilineStringsInInvoiceData(invoiceData)
 
-	// invoiceValues, err := readInvoice(invoiceFilePath)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Can't read invoice: %v\n", err)
-	// 	return
-	// }
+	invoiceTableAsArrayOfArrays, err := convertInvoiceTable(invoiceData[kInvoiceKey])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't parse invoice table: %v\n", err)
+		return
+	}
 
-	// template, err := readTemplate(flagTemplateFilePath)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Can't read template file: %v\n", err)
-	// 	return
-	// }
+	invoiceTableAsString := produceInvoiceTable(invoiceTableAsArrayOfArrays)
+	invoiceData[kGenInvoiceKey] = invoiceTableAsString
+	invoiceData[kGenDateCreatedKey] = invoiceData[kDateKey]
+	invoiceData[kGenDateDueKey] = invoiceData[kDateKey]
 
-	//fmt.Println(produceInvoiceTable(invoiceValues))
+	tmpl, err := template.ParseFiles(flagTemplateFilePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't create template: %v\n", err)
+		return
+	}
+
+	err = tmpl.Execute(os.Stdout, invoiceData)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't render template: %v\n", err)
+		return
+	}
 }
